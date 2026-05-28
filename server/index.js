eb19c8ad1bed0898467c31f968fc34362330aacb
RY-Cooper <112113950+RY-Cooper@users.noreply.github.com>
2026-05-28 16:31:26 -0700

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 
 const app = express();
 
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173' }));
 app.use(express.json());
